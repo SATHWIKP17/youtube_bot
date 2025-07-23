b93ef92d0efb578c25a2a52e09ddcefee425594e
SATHWIKP17 <sathwikreddypulimamidi051708@gmail.com>
2025-07-23 19:18:43 +0530

    NEw Commit

--- 24.py
+++ 24.py
@@ -11,36 +11,40 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 st.header("YOUTUBE BOT")
 q=st.text_input("Enter Your Query ")
+url=st.text_input("Enter The Url")
 lang=st.selectbox("Language",options=["HINDI","TELUGU","FRENCH","GREEK"])
 # os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
-try:
-    # app=
-    docs=YouTubeTranscriptApi.get_transcript("u6qDSFxY4iw")
-    docss=" ".join(doc['text'] for doc in docs)
-except TranscriptsDisabled:
-    print("Empty")
-except NoTranscriptFound:
-    print("Not Available")
-split=RecursiveCharacterTextSplitter(chunk_size=500)
-splitted=split.split_text(docss)
-embeddin=HuggingFaceEmbeddings(
-    model_name="sentence-transformers/paraphrase-MiniLM-L6-v2",
-)
 llm=ChatGroq(
     model="meta-llama/llama-4-scout-17b-16e-instruct",
     api_key="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"
 )
-print(q)
-cont=FAISS.from_texts(splitted,embedding=embeddin)
-retr=cont.as_retriever()
-da=retr.invoke(q)
-# context="\n\n".join(da)
-prompt=PromptTemplate(
-    template="Answer the question:{query} from the following paragraph in a deep way:{context} and translate it to the following language:{language}",
-    input_variables=['query','context','language']
-)
-llms=prompt|llm
-formatted_prompt = prompt.format(query=q, context=cont,language=lang)
-resp=llms.stream({"query":q,"context":da,"language":lang})
-print(resp)
-st.write_stream(r.content for r in resp)
+embeddin=HuggingFaceEmbeddings( 
+            model_name="sentence-transformers/paraphrase-MiniLM-L6-v2",
+        )
+def get_trans(url):
+    id=url.split("v=")[1]
+    docs=YouTubeTranscriptApi.get_transcript(id)
+    return " ".join(doc['text'] for doc in docs)
+if(st.button("Submit")):
+    if not url:
+        st.error("Enter Correct url")
+    else:
+        try:    
+            split=RecursiveCharacterTextSplitter(chunk_size=500)
+            splitted=split.split_text(get_trans(url))
+            cont=FAISS.from_texts(splitted,embedding=embeddin)
+            retr=cont.as_retriever()
+            da=retr.invoke(q)
+            prompt=PromptTemplate(
+                template="Answer the question:{query} from the following paragraph in a deep way:{context} and translate it to the following language:{language}",
+                input_variables=['query','context','language']
+            )
+            llms=prompt|llm
+            formatted_prompt = prompt.format(query=q, context=cont,language=lang)
+            resp=llms.stream({"query":q,"context":da,"language":lang})
+            st.write_stream(r.content for r in resp)
+        except TranscriptsDisabled:
+            print("Empty")
+        except NoTranscriptFound:
+            print("Not Available")
+
